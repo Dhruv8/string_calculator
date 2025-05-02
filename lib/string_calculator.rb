e2fa6778936ef9e4139de122122bcfb53a5e121b
Dhruv Sharma <dhruv@itsacheckmate.com>
2025-05-02 16:06:33 +0530

    Handle custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,14 @@ class StringCalculator
   def add(input)
     return 0 if input.strip.empty?
 
-    input.split(",").map(&:to_i).reduce(0) { |sum, number| sum + number }
-  end  
+    delimiters = [",", "\n"]
+
+    if input.start_with?("//")
+      delimiter_line, input = input.split("\n", 2)
+      custom_delimiter = delimiter_line[2] # assumes single-character delimiter
+      delimiters = [custom_delimiter]
+    end
+
+    input.split(Regexp.union(delimiters)).map(&:to_i).reduce(0, :+)
+  end
 end
