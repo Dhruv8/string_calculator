f47b81d1ca7afb94fc8148add94ce441c56b0be1
Dhruv Sharma <dhruv@itsacheckmate.com>
2025-05-02 15:39:38 +0530

    Handle Single String Input to return integer other then non-empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,8 @@
 
 class StringCalculator
   def add(input)
-    0
-  end
+    return 0 if input.strip.empty?
+
+    input.strip.to_i
+  end  
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe StringCalculator do
   it "returns 0 for an empty string" do
     expect(calc.add("")).to eq(0)
   end
+
+  it "returns the number itself for a single number" do
+    expect(calc.add("4")).to eq(4)
+  end
 end
